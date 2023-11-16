11f68fa1be7f39566de80798d83c3024e37db751
sureyeahdie <sureyeahdie@gmail.com>
2023-11-16 15:50:49 +0700

    dynamic import some components

--- core/modules/cms/components/cms-renderer/magezon/MagezonProduct/index.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonProduct/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable operator-linebreak */
 import Typography from '@common_typography';
-import Skeleton from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Skeleton';
-import ProductSlider from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Slider';
-import SingleProduct from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/SingleProduct';
+// import Skeleton from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Skeleton';
+// import ProductSlider from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Slider';
+// import SingleProduct from '@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/SingleProduct';
 import { generateQueries, getProductListConditions } from '@core_modules/cms/helpers/getProductListConditions';
 import { getProductList, getProductPrice } from '@core_modules/cms/services/graphql';
 import { useTranslation } from 'next-i18next';
@@ -14,6 +14,11 @@ import React, {
 import { useRouter } from 'next/router';
 import { priceVar } from '@root/core/services/graphql/cache';
 import { useReactiveVar } from '@apollo/client';
+import dynamic from 'next/dynamic';
+
+const Skeleton = dynamic(() => import('@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Skeleton'), { ssr: true });
+const ProductSlider = dynamic(() => import('@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/Slider'), { ssr: true });
+const SingleProduct = dynamic(() => import('@core_modules/cms/components/cms-renderer/magezon/MagezonProduct/SingleProduct'), { ssr: true });
 
 const MagezonProductList = (props) => {
     // prettier-ignore
