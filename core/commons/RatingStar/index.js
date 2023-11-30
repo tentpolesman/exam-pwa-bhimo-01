08e7c58fde8f02dcbf10480f48753f0550433b8f
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-11-30 14:28:11 +0700

    feat([29365467] - PWA - Commons - Rating Star): Percentage based fill star

--- core/commons/RatingStar/index.js
+++ core/commons/RatingStar/index.js
@@ -1,10 +1,10 @@
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import StarIcon from '@heroicons/react/24/solid/StarIcon';
 import cx from 'classnames';
 
-const RatingStar = ({
-    value = 1, maxvalue = 5, onChange = () => {}, disabled = true, sizeIcon = 'sm', miniSummary = false,
-}) => {
+// eslint-disable-next-line object-curly-newline
+const RatingStar = ({ value = 1, maxvalue = 5, onChange = () => {}, disabled = true, sizeIcon = 'sm', miniSummary = false }) => {
     const icon = [];
 
     const classes = cx('hover:cursor-pointer', {
@@ -25,15 +25,46 @@ const RatingStar = ({
         if (ind <= value) {
             icon.push(
                 <div role="button" className={cx('mr-0', 'p-0')} key={ind} disabled={disabled} onClick={() => onChange(ind)}>
-                    <StarIcon className={cx('text-yellow-400', classes)} />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={cx(classes, 'text-yellow-400')}>
+                        <path
+                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
                 </div>,
             );
         } else {
-            icon.push(
-                <div role="button" className={cx('mr-0', 'p-0')} key={ind} disabled={disabled} onClick={() => onChange(ind)}>
-                    <StarIcon className={cx('text-neutral-200', classes)} />
-                </div>,
-            );
+            // eslint-disable-next-line no-lonely-if
+            if (ind - value > 0 && ind - value < 1) {
+                icon.push(
+                    <div role="button" className={cx('mr-0', 'p-0')} key={ind} disabled={disabled} onClick={() => onChange(ind)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={cx(classes)}>
+                            <defs>
+                                <linearGradient id="grad2">
+                                    <stop offset={`${(value - (ind - 1)) * 100}%`} stopColor="#FBBF24" />
+                                    <stop offset={`${(value - (ind - 1)) * 100}%`} stopColor="#E5E7EB" />
+                                </linearGradient>
+                            </defs>
+                            <path
+                                fill="url(#grad2)"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>,
+                );
+            } else {
+                icon.push(
+                    <div role="button" className={cx('mr-0', 'p-0')} key={ind} disabled={disabled} onClick={() => onChange(ind)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={cx(classes, 'text-neutral-200')}>
+                            <path
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>,
+                );
+            }
         }
     }
     return <div className={cx('flex', 'flex-row')}>{icon.map((Item) => Item)}</div>;
