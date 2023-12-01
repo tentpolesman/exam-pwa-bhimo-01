47f272eddf262ddaa2d5fc780d0eeac44d41c366
revqiabdillah <rifqi.abdillah@sirclo.com>
2023-12-01 16:29:27 +0700

    feat([29365476]PWA - Tailwind : PLP): refactor PLP Using tailwind (layout onnly)

--- core/modules/catalog/plugins/ProductList/components/index.js
+++ core/modules/catalog/plugins/ProductList/components/index.js
@@ -1,336 +1,89 @@
-import GridList from '@common_gridlist';
-import Typography from '@common_typography';
-import { getLocalStorage, setLocalStorage } from '@helper_localstorage';
-import LabelView from '@plugin_productitem/components/LabelView';
-import PaginationSection from '@plugin_productlist/components/PaginationSection';
-import ProductItem from '@plugin_productitem/index';
-import useStyles from '@plugin_productlist/components/style';
-import classNames from 'classnames';
-import dynamic from 'next/dynamic';
-import React, { useCallback, useEffect, useState } from 'react';
+import Button from '@common/Button';
+import Select from '@common/Forms/Select';
+import React, { useState } from 'react';
+import GridIcon from '@heroicons/react/24/outline/Squares2X2Icon';
+import ListIcon from '@heroicons/react/24/outline/Bars3BottomLeftIcon';
+import FilterIcon from '@heroicons/react/24/outline/AdjustmentsHorizontalIcon';
+import Typography from '@common/Typography';
+import cx from 'classnames';
+import DrawerFilter from './DrawerFilter';
 
-const Filter = dynamic(() => import('@plugin_productlist/components/Filter'));
-const FilterDesktop = dynamic(() => import('@plugin_productlist/components/FilterDesktop'));
-const Sort = dynamic(() => import('@plugin_productlist/components/FilterDesktop/sort'));
-
-const ContentPagination = (props) => {
+const ViewProductList = (props) => {
     const {
-        query, showTabs, customFilter, elastic, t,
-        aggregations, setFiltervalue, category, defaultSort, config, TabView,
-        products, categoryPath, renderEmptyMessage, ProductListSkeleton, loading,
-        loadmore, dataTabs, onChangeTabs, onChangeCategory, page, totalPage, totalCount, handleChangePage, price, loadPrice, ...other
+        loading, loadmore, products,
     } = props;
-    const styles = useStyles();
-    const [isGrid, setGridState] = useState(true);
-    const { storeConfig } = props;
+    const [isGrid, setIsGrid] = useState('true');
+    const handleSetGrid = () => setIsGrid(!isGrid);
 
-    const setGrid = async (state) => {
-        setLocalStorage('isGrid', state);
-        setGridState(state);
-    };
+    const [openDrawerFilter, setOpenDrawerFilter] = useState(false);
+    const handleOpenDrawerFilter = () => setOpenDrawerFilter(true);
+    const handleCloseDrawetFilter = () => setOpenDrawerFilter(false);
 
     return (
-        <>
-            {showTabs ? (
-                <div className="hidden-desktop">
-                    <TabView
-                        // eslint-disable-next-line radix
-                        value={query.category_id ? query.category_id : 0}
-                        data={category}
-                        onChange={(e, value) => setFiltervalue({ ...query, ...{ category_id: value } })}
-                        {...other}
-                    />
+        <div className="flex flex-row gap-4 w-full">
+            <DrawerFilter open={openDrawerFilter} handleClose={handleCloseDrawetFilter} />
+            <div className="hidden lg:inline-flex flex-col lg:basis-1/4">
+                <div className="mb-5 h-[36px] border-neutral-100 border-b-[1px]">
+                    <Typography variant="h3" className="text-base basis-full">
+                        Shipping Options
+                    </Typography>
                 </div>
-            ) : null}
-            <div className={storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 'hidden-desktop' : ''}>
-                <Filter
-                    filter={customFilter || aggregations}
-                    defaultSort={JSON.stringify(defaultSort)}
-                    filterValue={query}
-                    setFiltervalue={setFiltervalue}
-                    isSearch={!!config.search}
-                    products={products}
-                    renderEmptyMessage={renderEmptyMessage}
-                    loading={loading}
-                    setGrid={(state) => setGrid(state)}
-                    t={t}
-                    onChangeCategory={onChangeCategory}
-                    {...other}
-                />
+                <div className="w-full bg-neutral-200 h-screen" />
             </div>
-            {storeConfig?.pwa?.drawer_filter_on_desktop_enable ? (
-                <div className={classNames(styles.filterBtnContainer, 'hidden-mobile')}>
-                    <Sort
-                        filter={customFilter || aggregations}
-                        defaultSort={JSON.stringify(defaultSort)}
-                        filterValue={query}
-                        setFiltervalue={setFiltervalue}
-                        isSearch={!!config.search}
-                        t={t}
-                        {...other}
-                    />
-                </div>
-            ) : null}
-
-            <div className="flex flex-row">
-                {storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                    ? (
-                        <div className="sm:basis-full lg:basis-2/12 hidden-mobile">
-                            <FilterDesktop
-                                filter={customFilter || aggregations}
-                                defaultSort={JSON.stringify(defaultSort)}
-                                filterValue={query}
-                                setFiltervalue={setFiltervalue}
-                                isSearch={!!config.search}
-                                products={products}
-                                renderEmptyMessage={renderEmptyMessage}
-                                loading={loading}
-                                tabs={dataTabs}
-                                t={t}
-                                onChangeTabs={onChangeTabs}
-                                storeConfig={storeConfig}
-                            />
-                        </div>
-                    )
-                    : null}
-                <div className={
-                        storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                            ? 'sm:basis-full xs:basis-full lg:basis-10/12'
-                            : 'sm:basis-full xs:basis-full lg:basis-full'
-                }
-                >
-                    {storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                        ? (
-                            <Typography variant="p" type="regular" className={classNames('hidden-mobile', styles.countProductTextDesktop)}>
-                                {products.total_count}
-                                {' '}
-                                {t('catalog:product:name')}
-                            </Typography>
-                        ) : null}
-                    <div className={styles.productContainer}>
-                        {/** Pagination List */}
-                        {loading && <ProductListSkeleton />}
-                        {!loadmore && !loading && (
-                            <GridList
-                                data={products.items}
-                                ItemComponent={ProductItem}
-                                itemProps={{
-                                    categorySelect: categoryPath,
-                                    LabelView,
-                                    isGrid,
-                                    catalogList: true,
-                                    className: 'grid-item',
-                                    price,
-                                    loadPrice,
-                                    ...other,
-                                }}
-                                gridItemProps={
-                                    isGrid
-                                        ? {
-                                            xs: 6, sm: 4, md: storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 3 : 2,
-                                        } : {
-                                            xs: 12, sm: 12, md: storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 12 : 12,
-                                        }
-                                }
-                            />
-                        )}
-                        <PaginationSection {...props} />
-                        {/** Pagination List */}
-                        <div className="latest-product-indicator" />
-                        {(products.items.length === products.total_count) || loading
-                            ? renderEmptyMessage(products.items.length, loading)
-                            : null}
+            <div className="lg:basis-3/4 basis-full flex flex-col">
+                <div className="flex flex-row items-center align-middle justify-between mb-5">
+                    <div className="flex flex-row items-center gap-2">
+                        <Button
+                            variant="outlined"
+                            icon={<FilterIcon />}
+                            iconPosition="left"
+                            className="lg:hidden h-[36px] flex items-center py-[8px] px-[12px] border-neutral-200"
+                            classNameText="text-neutral"
+                            onClick={handleOpenDrawerFilter}
+                        >
+                            Filters
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            icon={isGrid ? <GridIcon /> : <ListIcon />}
+                            iconPosition="right"
+                            className="h-[36px] flex items-center py-[8px] px-[12px] border-neutral-200"
+                            classNameText="text-neutral"
+                            onClick={handleSetGrid}
+                        >
+                            <Typography className="hidden lg:inline">View As</Typography>
+                        </Button>
                     </div>
+                    <Select
+                        options={[{ label: 'Tes', value: 'test' }]}
+                        placeholder="Sort By"
+                        className="h-[36px]"
+                        textFiledProps={{
+                            className: 'h-[36px] w-[117px] pl-2 mt-0',
+                        }}
+                        inputProps={{
+                            className: 'h-[34px] placeholder:text-neutral',
+                        }}
+                    />
+                </div>
+                <div className={cx(
+                    'grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3',
+                    {
+                        'grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1': !isGrid,
+                    },
+                )}
+                >
+                    {
+                        !loadmore && !loading && products.items && products.items.map((item) => (
+                            <div className="w-auto h-[350px]" key={item}>
+                                <div className="bg-neutral-100 w-full h-full" />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
-
-        </>
+        </div>
     );
 };
 
-const ContentLoadMore = (props) => {
-    const {
-        query, showTabs, customFilter, elastic, t,
-        aggregations, setFiltervalue, category, defaultSort, config, TabView,
-        products, categoryPath, renderEmptyMessage, ProductListSkeleton, loading,
-        loadmore, handleLoadMore, dataTabs, onChangeTabs, onChangeCategory, price, loadPrice, ...other
-    } = props;
-    const { storeConfig } = props;
-    const styles = useStyles();
-    const [isGrid, setGridState] = useState(true);
-    const [isExceedingOffset, setIsExceedingOffset] = useState(false);
-    const handleScroll = useCallback(() => {
-        // To get page offset of last user
-        // const lastUserLoaded = document.querySelector(`.grid-item:last-child`);
-        const lastUserLoaded = document.querySelector('.latest-product-indicator');
-        if (lastUserLoaded) {
-            const lastUserLoadedOffset = lastUserLoaded.offsetTop + lastUserLoaded.clientHeight;
-            const pageOffset = window.pageYOffset + window.innerHeight;
-
-            if (pageOffset > lastUserLoadedOffset) {
-                setIsExceedingOffset(true);
-            } else {
-                setIsExceedingOffset(false);
-            }
-        }
-    }, []);
-
-    React.useEffect(() => {
-        if (isExceedingOffset && !loadmore && products.items.length < products.total_count) {
-            handleLoadMore();
-        }
-    }, [isExceedingOffset]);
-
-    const setGrid = async (state) => {
-        setLocalStorage('isGrid', state);
-        setGridState(state);
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [handleScroll]);
-
-    useEffect(() => {
-        const gridView = getLocalStorage('isGrid');
-        if (gridView !== null) {
-            setGridState(gridView);
-        }
-    }, [isGrid]);
-
-    return (
-        <>
-            {showTabs ? (
-                <div className="hidden-desktop">
-                    <TabView
-                        // eslint-disable-next-line radix
-                        value={query.category_id ? query.category_id : 0}
-                        data={category}
-                        onChange={(e, value) => setFiltervalue({ ...query, ...{ category_id: value } })}
-                        {...other}
-                    />
-                </div>
-            ) : null}
-            <div className={storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 'hidden-desktop' : ''}>
-                <Filter
-                    filter={customFilter || aggregations}
-                    defaultSort={JSON.stringify(defaultSort)}
-                    filterValue={query}
-                    setFiltervalue={setFiltervalue}
-                    isSearch={!!config.search}
-                    products={products}
-                    renderEmptyMessage={renderEmptyMessage}
-                    loading={loading}
-                    setGrid={(state) => setGrid(state)}
-                    t={t}
-                    onChangeCategory={onChangeCategory}
-                    {...other}
-                />
-            </div>
-            {storeConfig?.pwa?.drawer_filter_on_desktop_enable ? (
-                <div className={classNames(styles.filterBtnContainer, 'hidden-mobile')}>
-                    <Sort
-                        filter={customFilter || aggregations}
-                        defaultSort={JSON.stringify(defaultSort)}
-                        filterValue={query}
-                        setFiltervalue={setFiltervalue}
-                        isSearch={!!config.search}
-                        t={t}
-                        {...other}
-                    />
-                </div>
-            ) : null}
-
-            <div className="flex flex-row">
-                {storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                    ? (
-                        <div className="sm:basis-full lg:basis-2/12 hidden-mobile">
-                            <FilterDesktop
-                                filter={customFilter || aggregations}
-                                defaultSort={JSON.stringify(defaultSort)}
-                                filterValue={query}
-                                setFiltervalue={setFiltervalue}
-                                isSearch={!!config.search}
-                                products={products}
-                                renderEmptyMessage={renderEmptyMessage}
-                                loading={loading}
-                                tabs={dataTabs}
-                                t={t}
-                                onChangeTabs={onChangeTabs}
-                                storeConfig={storeConfig}
-                            />
-                        </div>
-                    )
-                    : null}
-                <div className={
-                        storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                            ? 'sm:basis-full xs:basis-full lg:basis-10/12'
-                            : 'sm:basis-full xs:basis-full lg:basis-full'
-                }
-                >
-                    {storeConfig?.pwa?.drawer_filter_on_desktop_enable
-                        ? (
-                            <Typography variant="p" type="regular" className={classNames('hidden-mobile', styles.countProductTextDesktop)}>
-                                {products.total_count}
-                                {' '}
-                                {t('catalog:product:name')}
-                            </Typography>
-                        ) : null}
-                    <div className={styles.productContainer}>
-                        {loading && <ProductListSkeleton />}
-                        {!loading && (
-                            <GridList
-                                data={products.items}
-                                ItemComponent={ProductItem}
-                                itemProps={{
-                                    categorySelect: categoryPath,
-                                    LabelView,
-                                    isGrid,
-                                    catalogList: true,
-                                    className: 'grid-item',
-                                    price,
-                                    loadPrice,
-                                    ...other,
-                                }}
-                                gridItemProps={
-                                    isGrid
-                                        ? {
-                                            xs: 6, sm: 4, md: storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 3 : 2,
-                                        } : {
-                                            xs: 12, sm: 12, md: storeConfig?.pwa?.drawer_filter_on_desktop_enable ? 12 : 12,
-                                        }
-                                }
-                            />
-                        )}
-                        <div className="latest-product-indicator" />
-                        {(products.items.length === products.total_count) || loading
-                            ? renderEmptyMessage(products.items.length, loading)
-                            : null}
-                        <div className={loadmore ? styles.divLoadMore : styles.hideDivLoadMore}>
-                            <Typography align="center" variant="span" type="bold" letter="uppercase" color="gray">
-                                Loading
-                            </Typography>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    );
-};
-
-/**
- * Main Content
- * @param {*} props
- * @returns
- */
-const Content = (props) => {
-    const { isPagination } = props;
-    const MainContent = isPagination ? ContentPagination : ContentLoadMore;
-
-    return <MainContent {...props} />;
-};
-
-export default Content;
+export default ViewProductList;
