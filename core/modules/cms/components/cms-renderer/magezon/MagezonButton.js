874bcf5ffa707138891f35b291984436578fbd0d
sureyeahdie <sureyeahdie@gmail.com>
2023-12-06 15:06:46 +0700

    feat([29365475] CMS): adjust Magezon Button

--- core/modules/cms/components/cms-renderer/magezon/MagezonButton.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonButton.js
@@ -1,18 +1,33 @@
 /* eslint-disable react/no-danger */
-import React from 'react';
 import Button from '@common_button';
 import MagezonLink from '@core_modules/cms/components/cms-renderer/magezon/MagezonLink';
 import MagezonIcon from '@core_modules/cms/components/cms-renderer/magezon/MagezoneIcon';
-import useStyles from '@core_modules/cms/components/cms-renderer/magezon/style';
-import classNames from 'classnames';
+import cx from 'classnames';
 
 const MagezonButton = (props) => {
     const {
-        title, link, icon, icon_position, onClick = () => {},
-        button_align, button_color, button_border_radius,
+        title,
+        link,
+        icon,
+        icon_position,
+        onClick = () => {},
+        button_align,
+        button_color,
+        button_border_radius,
+        button_font_size,
+        button_border_style,
+        button_background_color,
+        button_border_color,
+        button_border_width,
+        button_size,
+        full_width,
+        button_style,
+        box_shadow_color,
+        gradient_color_1,
+        gradient_color_2,
     } = props;
-    const classes = useStyles(props);
     const wrapper = 'mgz-button';
+    const isButtonXs = button_size === 'xs';
     let isLeftIconPosition = '';
 
     if (icon_position) {
@@ -28,11 +43,16 @@ const MagezonButton = (props) => {
             {link && link !== '' ? (
                 <MagezonLink link={link}>
                     <Button
-                        style={{ borderRadius: button_border_radius || '0px' }}
-                        className={classNames(classes.button, 'mgz-btn')}
                         type="button"
                         align={`${button_align || 'left'}`}
                         onClick={onClick}
+                        className={cx('focus:shadow-none', {
+                            'w-full': full_width,
+                        })}
+                        classNameText={cx('mgz-button--text', {
+                            'text-xs': isButtonXs,
+                        })}
+                        size={isButtonXs ? 'sm' : button_size}
                     >
                         {icon && isLeftIconPosition ? <MagezonIcon icon={icon} icon_color={button_color} /> : null}
                         {title || ''}
@@ -40,19 +60,56 @@ const MagezonButton = (props) => {
                     </Button>
                 </MagezonLink>
             ) : (
-                <Button className={classes.button} type="button" align={`${button_align || 'left'}`} onClick={onClick}>
+                <Button
+                    type="button"
+                    align={`${button_align || 'left'}`}
+                    onClick={onClick}
+                    className={cx('focus:shadow-none', {
+                        'w-full': full_width,
+                    })}
+                    classNameText={cx('mgz-button--text', {
+                        'text-xs': isButtonXs,
+                    })}
+                    size={isButtonXs ? 'sm' : button_size}
+                >
                     {icon && isLeftIconPosition ? <MagezonIcon icon={icon} icon_color={button_color} /> : null}
                     {title || ''}
                     {icon && !isLeftIconPosition ? <MagezonIcon icon={icon} icon_color={button_color} /> : null}
                 </Button>
             )}
+            {/* eslint-disable */}
             <style jsx>
                 {`
-                    .mgz-btn {
-                        width: 100%;
+                    .mgz-button :global(.mgz-button--text) {
+                        ${button_color ? `color: ${button_color} !important;` : ''}
+                        ${button_font_size ? `font-size: ${button_font_size} !important;` : ''}
+                    }
+                    .mgz-button :global(.magezon-icon) {
+                        ${icon_position === 'right' ? 'margin-left: 5px;' : 'margin-right: 5px;'}
+                    }
+                    .mgz-button :global(button) {
+                        ${button_border_style ? `border-style: ${button_border_style} !important;` : ''}
+                        ${button_border_radius ? `border-radius: ${button_border_radius}px !important;` : ''}
+                        ${button_background_color ? `background-color: ${button_background_color} !important;` : ''}
+                        ${button_border_color ? `border-color: ${button_border_color} !important;` : ''}
+                        ${button_border_width ? `border-width: ${button_border_width}px !important;` : ''}
+                        ${button_style === '3d' && box_shadow_color ? `box-shadow: 0px 2px 4px -1px ${box_shadow_color}, 0px 4px 6px -1px ${box_shadow_color};` : ''}
+                        ${button_style === 'gradient' && (gradient_color_1 || gradient_color_2) ? `
+                            background-image: linear-gradient(to right, ${gradient_color_1} 0, ${gradient_color_2} 50%, ${gradient_color_1} 100%);
+                            background-size: 200% 100%;
+                        ` : ''}
+                    }
+                    .mgz-button :global(button:hover) {
+                        transition: all .2s ease-in-out;
+                        ${button_style === '3d' && box_shadow_color ? `box-shadow: 0px 1px 2px 0px ${box_shadow_color};` : ''}
+                        ${button_style === 'gradient' && (gradient_color_1 || gradient_color_2) ? `
+                            background-image: linear-gradient(to right, ${gradient_color_1} 0, ${gradient_color_2} 50%, ${gradient_color_1} 100%);
+                            background-position: 100% 0;
+                        ` : ''}
                     }
                 `}
             </style>
+            {/* eslint-enable */}
         </div>
     );
 };
