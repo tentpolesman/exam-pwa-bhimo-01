ace8c50567fb7f706a9f2bf7605f26494a43910e
abinowo <abinowo.ananta@gmail.com>
2023-12-08 15:29:12 +0700

    fix(29365467): adjustment common button

--- core/commons/Button/index.js
+++ core/commons/Button/index.js
@@ -1,14 +1,19 @@
-import Show from '@common_show';
+/* eslint-disable max-len */
+import cx from 'classnames';
 import Typography from '@common_typography';
 import ArrowPath from '@heroicons/react/24/outline/ArrowPathIcon';
-import cx from 'classnames';
 import propTypes from 'prop-types';
+import Show from '@common_show';
+import Link from 'next/link';
 
 const Button = (props) => {
     const {
         className = '',
         classNameText = '',
         variant = 'primary',
+        link,
+        linkTarget,
+        linkPassHref,
         children,
         disabled = false,
         onClick = () => {},
@@ -20,14 +25,19 @@ const Button = (props) => {
         textProps = {},
         iconProps = {},
         customChildren,
+        underline,
         ...restProps
     } = props;
 
+    const { className: classIcon, ...resIconProps } = iconProps;
+    const isPositionRight = position === 'right';
+    const isDisableOrLoading = disabled || loading;
+
     const buttonSizes = {
-        sm: `px-[16px] py-[8px] ${iconOnly ? '!p-[8px]' : ''}`,
-        md: `px-[20px] py-[10px] ${iconOnly ? '!p-[10px]' : ''}`,
-        lg: `px-[22px] py-[12px] ${iconOnly ? '!p-[10px]' : ''}`,
-        xl: `px-[26px] py-[14px] ${iconOnly ? '!p-[12px]' : ''}`,
+        sm: `px-[16px] py-[8px] ${iconOnly ? 'p-[8px]' : ''}`,
+        md: `px-[20px] py-[10px] ${iconOnly ? 'p-[10px]' : ''}`,
+        lg: `px-[22px] py-[12px] ${iconOnly ? 'p-[10px]' : ''}`,
+        xl: `px-[26px] py-[14px] ${iconOnly ? 'p-[12px]' : ''}`,
     };
 
     const textVariants = {
@@ -37,78 +47,128 @@ const Button = (props) => {
         xl: 'bd-1',
     };
 
-    const classes = cx('focus:shadow-[0_0_0_4px]', 'rounded-md', buttonSizes[size], className);
+    const classes = cx('rounded-md', buttonSizes[size], className);
 
-    const { className: classIcon, ...resIconProps } = iconProps;
+    const styleClass = {
+        primary: {
+            button: cx(
+                'focus:shadow-[0_0_0_4px] hover:shadow-lg bg-pwa-button_background hover:bg-pwa-button_background_hover focus:shadow-primary-300 ',
+                'active:bg-primary disabled:bg-neutral-200 disabled:text-neutral-400 disabled:hover:shadow-none',
+                'disabled:focus:shadow-none disabled:active:bg-neutral-200 disabled:active:shadow-none',
+            ),
+            typography: cx('!text-pwa-button_text', underline && 'underline'),
+        },
+        secondary: {
+            button: 'focus:shadow-[0_0_0_4px] hover:shadow-lg bg-primary-100 hover:bg-primary-200 focus:shadow-primary-300 active:bg-primary-200',
+            typography: cx('!text-primary group-active:!text-neutral-white', underline && 'underline'),
+        },
+        tertiary: {
+            button: cx(
+                'focus:shadow-[0_0_0_4px] hover:shadow-lg bg-neutral-white hover:shadow-lg',
+                'focus:shadow-primary-300 active:shadow-primary-300 active:shadow-[0_0_0_4px]',
+            ),
+            typography: cx('!text-primary', underline && 'underline'),
+        },
+        outlined: {
+            button: cx(
+                'focus:shadow-[0_0_0_4px] hover:shadow-lg bg-neutral-white border border-black',
+                'hover:opacity-50 focus:shadow-neutral-100 focus:border-none',
+            ),
+            typography: cx('!text-black', underline && 'underline'),
+        },
+        disabled: {
+            button: cx(
+                'focus:shadow-[0_0_0_4px] hover:shadow-lg bg-neutral-100 hover:bg-neutral-100',
+                'hover:shadow-none focus:shadow-none active:bg-neutral-100 active:shadow-none',
+            ),
+            typography: cx('!text-neutral-white', underline && 'underline'),
+        },
+        plain: {
+            button: '',
+            typography: cx('!text-black hover:text-neutral-400', underline && 'underline'),
+        },
+    };
 
-    const primaryClass = `
-    bg-pwa-button_background hover:bg-pwa-button_background_hover focus:shadow-primary-300 active:bg-primary
-    disabled:bg-neutral-200 disabled:text-neutral-400 disabled:hover:shadow-none
-    disabled:focus:shadow-none disabled:active:bg-neutral-200 disabled:active:shadow-none`;
+    const ButtonContent = () => (
+        <Typography
+            variant={textVariants[size]}
+            className={
+                cx(
+                    'flex',
+                    'items-center',
+                    classNameText,
+                    styleClass[variant].typography,
+                    isDisableOrLoading && styleClass.disabled.typography,
+                    {
+                        'flex-row-reverse': icon && isPositionRight,
+                    },
+                )
+            }
+            {...textProps}
+        >
+            <Show when={icon && loading}>
+                <ArrowPath
+                    className={cx(
+                        'animate-spin w-6 h-6',
+                        {
+                            'ml-[6px]': isPositionRight && !iconOnly,
+                            'mr-[6px]': !isPositionRight && !iconOnly,
+                            'text-lg': !iconOnly || (iconOnly && (size === 'sm' || size === 'md')),
+                        },
+                    )}
+                />
+            </Show>
+            <Show when={icon && !loading}>
+                {icon ? React.cloneElement(icon, {
+                    className: cx(
+                        'w-6 h-6',
+                        {
+                            'mr-[6px]': !isPositionRight && !iconOnly,
+                            'ml-[6px]': isPositionRight && !iconOnly,
+                        },
+                        classIcon,
+                    ),
+                    ...resIconProps,
+                }) : null}
+            </Show>
+            <Show when={!iconOnly}>{children}</Show>
+        </Typography>
+    );
+
+    if (link) {
+        return (
+            <Link
+                href={link}
+                target={linkTarget || '_self'}
+                passHref={linkPassHref}
+                className={cx(
+                    'group',
+                    'flex items-center',
+                    !isDisableOrLoading && styleClass[variant].button,
+                    isDisableOrLoading && styleClass.disabled.button,
+                    classes,
+                )}
+            >
+                <ButtonContent />
+                {customChildren && customChildren}
+            </Link>
+        );
+    }
 
     return (
         <button
             type="button"
             onClick={onClick}
-            disabled={disabled || loading}
+            disabled={isDisableOrLoading}
             className={cx(
                 'group',
-                'hover:shadow-lg',
-                variant === 'primary' && primaryClass,
-                {
-                    'bg-pwa-button_background hover:bg-pwa-button_background_hover focus:shadow-primary-300 active:bg-primary': variant === 'primary',
-                    'bg-primary-100 hover:bg-primary-200 focus:shadow-primary-300 active:bg-primary-200': variant === 'secondary',
-                    // eslint-disable-next-line max-len
-                    'bg-neutral-white hover:shadow-lg focus:shadow-primary-300 active:shadow-primary-300 active:shadow-[0_0_0_4px]':
-                        variant === 'tertiary',
-                    'bg-neutral-white border border-black hover:opacity-50 focus:shadow-neutral-100 focus:border-none': variant === 'outlined',
-                },
+                !isDisableOrLoading && styleClass[variant].button,
+                isDisableOrLoading && styleClass.disabled.button,
                 classes,
             )}
             {...restProps}
         >
-            <Typography
-                variant={textVariants[size]}
-                className={cx(
-                    'flex',
-                    'items-center',
-                    'justify-center',
-                    {
-                        '!text-pwa-button_text': variant === 'primary',
-                        '!text-primary': variant === 'secondary' || variant === 'tertiary',
-                        '!text-black': variant === 'outlined',
-                        '!text-neutral-white': disabled,
-                        'group-active:!text-neutral-white': variant === 'secondary',
-                        'flex-row-reverse': icon && position === 'right',
-                    },
-                    classNameText,
-                )}
-                {...textProps}
-            >
-                <Show when={icon && loading}>
-                    <ArrowPath
-                        className={cx('animate-spin w-6 h-6', {
-                            'mr-[6px]': position !== 'right' && !iconOnly,
-                            'ml-[6px]': position === 'right' && !iconOnly,
-                            'text-lg': !iconOnly || (iconOnly && (size === 'sm' || size === 'md')),
-                        })}
-                    />
-                </Show>
-                <Show when={icon && !loading}>
-                    {icon ? React.cloneElement(icon, {
-                        className: cx(
-                            'w-6 h-6',
-                            {
-                                'mr-[6px]': position !== 'right' && !iconOnly,
-                                'ml-[6px]': position === 'right' && !iconOnly,
-                            },
-                            classIcon,
-                        ),
-                        ...resIconProps,
-                    }) : null}
-                </Show>
-                <Show when={!iconOnly}>{children}</Show>
-            </Typography>
+            <ButtonContent />
             {customChildren && customChildren}
         </button>
     );
@@ -116,20 +176,24 @@ const Button = (props) => {
 
 Button.propTypes = {
     className: propTypes.string,
-    iconOnly: propTypes.bool,
-    variant: propTypes.oneOf(['primary', 'secondary', 'tertiary', 'outlined']),
+    variant: propTypes.oneOf(['primary', 'secondary', 'tertiary', 'outlined', 'plain']),
     children: propTypes.any,
     disabled: propTypes.bool,
     onClick: propTypes.func,
     loading: propTypes.bool,
     size: propTypes.oneOf(['sm', 'md', 'lg', 'xl']),
-    icon: propTypes.element,
+    icon: propTypes.oneOfType([propTypes.element, propTypes.bool]),
+    iconOnly: propTypes.bool,
+    iconProps: propTypes.object,
     iconPosition: propTypes.oneOf(['left', 'right']),
+    underline: propTypes.bool,
+    link: propTypes.string,
+    linkTarget: propTypes.string,
+    linkPassHref: propTypes.bool,
 };
 
 Button.defaultProps = {
     className: '',
-    iconOnly: false,
     variant: 'primary',
     children: '',
     disabled: false,
@@ -137,7 +201,13 @@ Button.defaultProps = {
     loading: false,
     size: 'md',
     icon: undefined,
+    iconOnly: false,
+    iconProps: {},
     iconPosition: 'left',
+    underline: false,
+    link: '',
+    linkTarget: '_self',
+    linkPassHref: true,
 };
 
 export default Button;
