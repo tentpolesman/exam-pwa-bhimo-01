963610b5d2ca829a262b0b98f33841e202884d6e
sureyeahdie <sureyeahdie@gmail.com>
2023-12-08 16:09:21 +0700

    feat([29365467] Commons): Removed Slick from Carousel component, Adjusted ContainerScroll component

--- core/commons/ContainerScroll/index.js
+++ core/commons/ContainerScroll/index.js
@@ -6,7 +6,6 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
 const ContainerScroll = ({
     variant = 'horizontal',
-    itemsLength = 10,
     className,
     children,
     maxHeight = '100%',
@@ -18,19 +17,20 @@ const ContainerScroll = ({
     const isHorizontal = variant === 'horizontal';
     const isVertical = variant === 'vertical';
 
+    // calculates the width of the first child from the list rendered
+    const containerChildrenWidth = containerRef?.current?.children[0]?.clientWidth || 0;
+
     const onClickArrowLeft = () => {
-        const scrollTo = containerRef.current.clientWidth / itemsLength;
-        containerRef.current.scrollLeft -= scrollTo;
+        containerRef.current.scrollLeft -= containerChildrenWidth;
     };
 
     const onClickArrowRight = () => {
-        const scrollTo = containerRef.current.clientWidth / itemsLength;
-        containerRef.current.scrollLeft += scrollTo;
+        containerRef.current.scrollLeft += containerChildrenWidth;
     };
 
     return (
         <div
-            className="container-scroll relative"
+            className={cx('container-scroll relative group')}
             style={{
                 ...(maxHeight ? { maxHeight } : null),
                 ...(maxWidth ? { maxWidth } : null),
@@ -38,17 +38,20 @@ const ContainerScroll = ({
         >
             <Show when={showArrow}>
                 <div
-                    style={{ zIndex: 999 }}
                     className={cx(
                         'container-scroll-arrow flex justify-between w-[100%]',
                         'px-[5px]',
                         'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
+                        'opacity-0',
+                        'group-hover:opacity-100',
+                        'z-[1]',
+                        'pointer-events-none',
                     )}
                 >
-                    <Button variant="tertiary" className="container-scroll-arrow-left !px-[10px]" onClick={onClickArrowLeft}>
+                    <Button variant="tertiary" className="container-scroll-arrow-left !px-[10px] pointer-events-auto" onClick={onClickArrowLeft}>
                         <ChevronLeftIcon style={{ width: arrowSize, height: arrowSize }} />
                     </Button>
-                    <Button variant="tertiary" className="container-scroll-arrow-right !px-[10px]" onClick={onClickArrowRight}>
+                    <Button variant="tertiary" className="container-scroll-arrow-right !px-[10px] pointer-events-auto" onClick={onClickArrowRight}>
                         <ChevronRightIcon style={{ width: arrowSize, height: arrowSize }} />
                     </Button>
                 </div>
@@ -59,15 +62,17 @@ const ContainerScroll = ({
                     ...(maxHeight ? { maxHeight } : null),
                     ...(maxWidth ? { maxWidth } : null),
                 }}
-                className={
-                    cx(
-                        'container-scroll-data',
-                        'relative',
-                        isHorizontal && 'overflow-x-auto flex scroll-smooth',
-                        isVertical && 'overflow-y-auto',
-                        className,
-                    )
-                }
+                className={cx(
+                    'w-full',
+                    'container-scroll-data',
+                    'relative',
+                    'scrollbar-none',
+                    'p-1',
+                    '-m-1',
+                    isHorizontal && 'overflow-x-auto flex scroll-smooth',
+                    isVertical && 'overflow-y-auto',
+                    className,
+                )}
             >
                 {children}
             </div>
