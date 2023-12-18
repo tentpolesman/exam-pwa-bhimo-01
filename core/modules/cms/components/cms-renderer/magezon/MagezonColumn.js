23e8f126fa39b1f5928093e0e45bcb475e43bf34
sureyeahdie <sureyeahdie@gmail.com>
2023-12-18 09:09:32 +0700

    move element inner styling to magezon root file

--- core/modules/cms/components/cms-renderer/magezon/MagezonColumn.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonColumn.js
@@ -21,21 +21,6 @@ const MagezonColumn = (props) => {
         lg_hide,
         xl_hide,
         storeConfig,
-        border_style,
-        background_position,
-        background_color,
-        border_top_width,
-        border_right_width,
-        border_bottom_width,
-        border_left_width,
-        margin_top,
-        margin_right,
-        margin_bottom,
-        margin_left,
-        padding_top,
-        padding_right,
-        padding_bottom,
-        padding_left,
     } = props;
     let classColumn = 'mgz-column flex flex-col ';
     if (xs_size && xs_size !== '') classColumn += `${getFlexBasisTailwind(xs_size, 'xs')} `;
@@ -72,13 +57,6 @@ const MagezonColumn = (props) => {
                 {`
                     .mgz-column {
                         width: 100%;
-                        border-style: ${border_style || 'solid'};
-                        background-position: ${background_position?.split('-').join(' ')};
-                        background-color: ${background_color || 'transparent'};
-                        border-width: ${border_top_width || 0}px ${border_right_width || 0}px ${border_bottom_width || 0}px
-                            ${border_left_width || 0}px;
-                        margin: ${margin_top || 0}px ${margin_right || 0}px ${margin_bottom || 0}px ${margin_left || 0}px;
-                        padding: ${padding_top || 0}px ${padding_right || 0}px ${padding_bottom || 0}px ${padding_left || 0}px;
                     }
                 `}
             </style>
--- core/modules/cms/components/cms-renderer/magezon/MagezonRow.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonRow.js
@@ -47,13 +47,6 @@ const MagezonRow = (props) => {
                 {`
                     .mgz-row {
                         width: 100%;
-                        border-style: ${border_style || 'solid'};
-                        background-position: ${background_position?.split('-').join(' ')};
-                        background-color: ${background_color || 'transparent'};
-                        border-width: ${border_top_width || 0}px ${border_right_width || 0}px ${border_bottom_width || 0}px
-                            ${border_left_width || 0}px;
-                        margin: ${margin_top || 0}px ${margin_right || 0}px ${margin_bottom || 0}px ${margin_left || 0}px;
-                        padding: ${padding_top || 0}px ${padding_right || 0}px ${padding_bottom || 0}px ${padding_left || 0}px;
                     }
                     .mgz-element-row-max-width {
                         width: ${max_width}px;
