d31a787023aaaeb96c3beac6121e64b458cfcfe4
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-12-22 09:14:07 +0700

    feat([29365480] - PWA - My Account): Fix SSR, fix empty message order/address on my account dashboard

--- pages/customer/account/address.js
+++ pages/customer/account/address.js
@@ -1,14 +1,6 @@
-import Page from '@core_modules/customer/pages/address/';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Page from '@core_modules/customer/pages/address';
+import getSSRProps from '@core_modules/customer/pages/address/ssr';
 
-export async function getServerSideProps(ctx) {
-    return {
-        props: {
-            ...(await serverSideTranslations(ctx.locale, ['common', 'customer', 'otp', 'validate', 'login'])),
-            withAuth: true,
-            query: ctx.query,
-        },
-    };
-}
+export const getServerSideProps = async (ctx) => getSSRProps(ctx);
 
 export default Page;
