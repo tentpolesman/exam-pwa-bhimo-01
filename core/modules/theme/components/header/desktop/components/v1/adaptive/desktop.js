3b0c1ad317abd25d5e55b7b8f65a88d8a0dc8a4a
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-12-26 09:05:58 +0700

    feat([29365480] - PWA - Header): Remove unused codes

--- core/modules/theme/components/header/desktop/components/v1/adaptive/desktop.js
+++ core/modules/theme/components/header/desktop/components/v1/adaptive/desktop.js
@@ -227,47 +227,6 @@ const DesktopHeader = (props) => {
                                 )}
                             >
                                 <UserInfo t={t} isLogin={isLogin} customer={customer} open={open} setOpen={setOpen} PopoverContent={PopoverContent} />
-                                {/* {isLogin ? (
-                                    <div onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
-                                        <Popover
-                                            content={<PopoverContent />}
-                                            open={open}
-                                            setOpen={setOpen}
-                                            className={cx('w-[160px]', 'top-[99%]', 'left-[0%]')}
-                                            contentClassName={cx('rounded-sm')}
-                                        >
-                                            <div className="popover-children">
-                                                <UserIcon className={cx('w-[24px]', 'text-neutral-600', 'inline-block', 'ml-1')} />
-                                                {customer && customer.firstname && (
-                                                    <Typography variant="bd-2b" className={cx('inline-block', 'pl-2', '!text-primary-700')}>
-                                                        {customer.firstname.length <= 12
-                                                            ? `${customer.firstname}`
-                                                            : `${customer.firstname.substring(0, 10)}...`}
-                                                    </Typography>
-                                                )}
-                                            </div>
-                                        </Popover>
-                                    </div>
-                                ) : (
-                                    <Link href="/customer/account/login">
-                                        <UserIcon
-                                            className={cx(
-                                                'w-[24px]',
-                                                'text-neutral-600',
-                                                'hover:text-primary-700',
-                                                'inline-block',
-                                                'ml-1',
-                                                'group-hover:text-primary-700',
-                                            )}
-                                        />
-                                        <Typography
-                                            variant="bd-2b"
-                                            className={cx('inline-block', 'pl-2', 'hover:text-primary-700', 'group-hover:text-primary-700')}
-                                        >
-                                            {t('common:menu:sign')}/{t('common:menu:register')}
-                                        </Typography>
-                                    </Link>
-                                )} */}
                             </div>
                         </div>
                     </div>
