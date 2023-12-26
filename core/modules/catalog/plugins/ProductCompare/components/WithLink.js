7574d67fc3cad3729f1632037ec77452706fb1d1
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-12-26 08:48:50 +0700

    feat([29365480] - PWA - Header): Revamp header styling

--- core/modules/catalog/plugins/ProductCompare/components/WithLink.js
+++ core/modules/catalog/plugins/ProductCompare/components/WithLink.js
@@ -5,25 +5,33 @@ import ArrowsRightLeftIcon from '@heroicons/react/24/solid/ArrowsRightLeftIcon';
 import cx from 'classnames';
 import Link from 'next/link';
 
-const WithLink = ({ compareList, handleLink, isLogin }) => {
-    if (compareList && compareList.compareList && compareList.compareList.item_count && isLogin) {
+const WithLink = ({ compareList, handleLink }) => {
+    if (compareList && compareList.compareList && compareList.compareList.item_count) {
         return (
-            <Link href={handleLink} prefetch={false}>
+            <Link href={handleLink} prefetch={false} className={cx('group')}>
                 {compareList ? (
                     <BadgeCounter value={compareList.compareList.item_count > 0 ? compareList.compareList.item_count : 0}>
-                        <ArrowsRightLeftIcon className={cx('w-[24px]', 'text-neutral-600', 'mt-3')} />
+                        <ArrowsRightLeftIcon
+                            className={cx('w-[24px]', 'text-neutral-600', 'hover:text-primary-700', 'group-hover:text-primary-700', 'mt-3')}
+                        />
                     </BadgeCounter>
                 ) : (
                     <BadgeCounter value={0}>
-                        <ArrowsRightLeftIcon className={cx('w-[24px]', 'text-neutral-600', 'mt-3')} />
+                        <ArrowsRightLeftIcon
+                            className={cx('w-[24px]', 'text-neutral-600', 'hover:text-primary-700', 'group-hover:text-primary-700', 'mt-3')}
+                        />
                     </BadgeCounter>
                 )}
             </Link>
         );
     }
     return (
-        <Link href={handleLink} prefetch={false}>
-            <ArrowsRightLeftIcon className={cx('w-[24px]', 'text-neutral-600', 'mt-3')} />
+        <Link href={handleLink} prefetch={false} className={cx('group')}>
+            <BadgeCounter value={0}>
+                <ArrowsRightLeftIcon
+                    className={cx('w-[24px]', 'text-neutral-600', 'hover:text-primary-700', 'group-hover:text-primary-700', 'mt-3')}
+                />
+            </BadgeCounter>
         </Link>
     );
 };
