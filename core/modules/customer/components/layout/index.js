fcd3a9fe4ba4f30b50a01a2e8d206a0e0f0b22f3
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-12-27 12:07:13 +0700

    feat([29365480] - PWA - My Account): Add layout on mobile and tablet for accordion my account

--- core/modules/customer/components/layout/index.js
+++ core/modules/customer/components/layout/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-plusplus */
+import Accordion from '@common/Accordion';
 import Typography from '@common_typography';
+
 import { modules } from '@config';
 import cx from 'classnames';
 import Link from 'next/link';
@@ -57,47 +59,66 @@ const Layout = (props) => {
         }
     }
     return (
-        <div className="flex flex-row desktop:gap-x-[18px]">
-            <div className={cx('md:basis-3/12', 'xs:basis-full', 'mobile:max-desktop:hidden')}>
-                <div className={cx('p-3', 'rounded-lg', 'border-[1px]', 'border-neutral-200', 'shadow-sm')}>
-                    <ul>
-                        {menu.map((val, idx) => {
-                            if (idx === 0) {
-                                return (
-                                    <li
-                                        key={idx}
-                                        className={cx(
-                                            'mx-3',
-                                            'pt-3',
-                                            'pb-5',
-                                            'text-[18px]',
-                                            'font-semibold',
-                                            'leading-7',
-                                            'border-b-[1px]',
-                                            'border-neutral-200',
-                                        )}
-                                    >
-                                        {val.title}
-                                    </li>
-                                );
-                            }
-                            if (idx === 1) {
-                                return (
-                                    <li
-                                        key={idx}
-                                        className={cx('px-3', 'mt-3', 'pt-2', 'pb-2', 'text-md', 'rounded-md', {
-                                            'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
-                                                router.asPath === '/customer/account' || activeMenu === '/customer/account',
-                                        })}
-                                    >
-                                        <Link href="/customer/account">{val.title}</Link>
-                                    </li>
-                                );
-                            }
-                            if (val.title === t('customer:menu:return')) {
-                                return (
-                                    <>
-                                        <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
+        <>
+            <div className="grid grid-cols-1 desktop:hidden">
+                <div className={cx('mobile:max-tablet:px-4')}>
+                    <Accordion
+                        label={<Typography className={cx('!text-lg', 'font-semibold', 'leading-7')}>{title || titlePage}</Typography>}
+                        classLabel={cx('p-2')}
+                        classSummary={cx('p-3', 'border-[1px]', 'border-neutral-100', 'rounded-lg', 'shadow-sm')}
+                    >
+                        <div className={cx('w-full', 'px-5', 'border-[2px]', 'border-neutral-100', 'rounded-lg')}>
+                            <ul>
+                                {menu.map((val, idx) => {
+                                    if (idx === 0) {
+                                        return null;
+                                    }
+                                    if (idx === 1) {
+                                        return (
+                                            <li
+                                                key={idx}
+                                                className={cx('px-3', 'mt-3', 'pt-2', 'pb-2', 'text-md', 'rounded-md', {
+                                                    'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                        router.asPath === '/customer/account' || activeMenu === '/customer/account',
+                                                })}
+                                            >
+                                                <Link href="/customer/account">{val.title}</Link>
+                                            </li>
+                                        );
+                                    }
+                                    if (val.title === t('customer:menu:return')) {
+                                        return (
+                                            <>
+                                                <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
+                                                <li
+                                                    key={idx}
+                                                    className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
+                                                        'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                            router.asPath === val.href || activeMenu === val.href,
+                                                    })}
+                                                >
+                                                    <Link href={val.href}>{val.title}</Link>
+                                                </li>
+                                            </>
+                                        );
+                                    }
+                                    if (modules.productreview.enabled && val.title === t('customer:menu:myProductReview')) {
+                                        return (
+                                            <>
+                                                <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
+                                                <li
+                                                    key={idx}
+                                                    className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
+                                                        'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                            router.asPath === val.href || activeMenu === val.href,
+                                                    })}
+                                                >
+                                                    <Link href={val.href}>{val.title}</Link>
+                                                </li>
+                                            </>
+                                        );
+                                    }
+                                    return (
                                         <li
                                             key={idx}
                                             className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
@@ -107,47 +128,116 @@ const Layout = (props) => {
                                         >
                                             <Link href={val.href}>{val.title}</Link>
                                         </li>
-                                    </>
-                                );
-                            }
-                            if (modules.productreview.enabled && val.title === t('customer:menu:myProductReview')) {
-                                return (
-                                    <>
-                                        <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
-                                        <li
-                                            key={idx}
-                                            className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
-                                                'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
-                                                    router.asPath === val.href || activeMenu === val.href,
-                                            })}
-                                        >
-                                            <Link href={val.href}>{val.title}</Link>
-                                        </li>
-                                    </>
-                                );
-                            }
-                            return (
-                                <li
-                                    key={idx}
-                                    className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
-                                        'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
-                                            router.asPath === val.href || activeMenu === val.href,
-                                    })}
-                                >
-                                    <Link href={val.href}>{val.title}</Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    </Accordion>
+                </div>
+                <div>
+                    <Typography
+                        variant="h2"
+                        type="bold"
+                        letter="capitalize"
+                        className={cx('pl-0', 'mobile:max-desktop:mt-6', 'mobile:max-tablet:px-4', 'mobile:max-tablet:hidden')}
+                    >
+                        {title || titlePage}
+                    </Typography>
+                    {children}
                 </div>
             </div>
-            <div className="md:basis-9/12 xs:basis-full sm:basis-full">
-                <Typography variant="h2" type="bold" letter="capitalize" className={cx('mobile:max-desktop:hidden', 'pl-0')}>
-                    {title || titlePage}
-                </Typography>
-                {children}
+            <div className="flex flex-row desktop:gap-x-[18px] mobile:max-desktop:hidden">
+                <div className={cx('desktop:basis-3/12', 'xs:basis-full', 'mobile:max-desktop:hidden')}>
+                    <div className={cx('p-3', 'rounded-lg', 'border-[1px]', 'border-neutral-200', 'shadow-sm')}>
+                        <ul>
+                            {menu.map((val, idx) => {
+                                if (idx === 0) {
+                                    return (
+                                        <li
+                                            key={idx}
+                                            className={cx(
+                                                'mx-3',
+                                                'pt-3',
+                                                'pb-5',
+                                                'text-[18px]',
+                                                'font-semibold',
+                                                'leading-7',
+                                                'border-b-[1px]',
+                                                'border-neutral-200',
+                                            )}
+                                        >
+                                            {val.title}
+                                        </li>
+                                    );
+                                }
+                                if (idx === 1) {
+                                    return (
+                                        <li
+                                            key={idx}
+                                            className={cx('px-3', 'mt-3', 'pt-2', 'pb-2', 'text-md', 'rounded-md', {
+                                                'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                    router.asPath === '/customer/account' || activeMenu === '/customer/account',
+                                            })}
+                                        >
+                                            <Link href="/customer/account">{val.title}</Link>
+                                        </li>
+                                    );
+                                }
+                                if (val.title === t('customer:menu:return')) {
+                                    return (
+                                        <>
+                                            <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
+                                            <li
+                                                key={idx}
+                                                className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
+                                                    'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                        router.asPath === val.href || activeMenu === val.href,
+                                                })}
+                                            >
+                                                <Link href={val.href}>{val.title}</Link>
+                                            </li>
+                                        </>
+                                    );
+                                }
+                                if (modules.productreview.enabled && val.title === t('customer:menu:myProductReview')) {
+                                    return (
+                                        <>
+                                            <div className={cx('mx-2', 'my-2', 'h-[1px]', 'bg-neutral-200')} />
+                                            <li
+                                                key={idx}
+                                                className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
+                                                    'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                        router.asPath === val.href || activeMenu === val.href,
+                                                })}
+                                            >
+                                                <Link href={val.href}>{val.title}</Link>
+                                            </li>
+                                        </>
+                                    );
+                                }
+                                return (
+                                    <li
+                                        key={idx}
+                                        className={cx('px-3', 'py-2', 'text-md', 'rounded-md', {
+                                            'bg-neutral-50 border-[1px] border-neutral-100 font-medium':
+                                                router.asPath === val.href || activeMenu === val.href,
+                                        })}
+                                    >
+                                        <Link href={val.href}>{val.title}</Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+                <div className="desktop:basis-9/12 xs:basis-full sm:basis-full">
+                    <Typography variant="h2" type="bold" letter="capitalize" className={cx('mobile:max-desktop:hidden', 'pl-0')}>
+                        {title || titlePage}
+                    </Typography>
+                    {children}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
