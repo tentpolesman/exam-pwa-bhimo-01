a9cb15e9f14ed7d261486db2118b09f5a1ccb439
revqiabdillah <rifqi.abdillah@sirclo.com>
2024-01-03 14:04:35 +0700

    29365467 - Update feedback bug, script, and readme

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = withPWA({
             },
             {
                 protocol: 'https',
-                hostname: baseHostUrl.replace('http://', '').replace('https://', '').replace('/graphql'),
+                hostname: baseHostUrl.replace('http://', '').replace('https://', '').replace('/graphql', ''),
                 port: '',
                 pathname: '/**',
             },
