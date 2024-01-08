36f9d212dcc2eca0003d2dec13b58962c42f6bbb
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2024-01-08 12:25:31 +0700

    feat([29365480] - PWA - My Account): Fix loader in my account dashboard - Feedback 268

--- core/modules/customer/pages/account/components/Skeleton/index.js
+++ core/modules/customer/pages/account/components/Skeleton/index.js
@@ -5,27 +5,24 @@ import cx from 'classnames';
 const SkeletonLoader = () => (
     <>
         <div className={cx('flex', 'flex-row', 'tablet:border-t-[1px]', 'tablet:border-neutral-200', 'tablet:mt-3')}>
-            <div className="mobile:max-tablet:basis-full desktop:basis-10/12 pt-5">
+            <div className="mobile:max-desktop:basis-full desktop:basis-10/12 pt-5">
                 <Skeleton
                     animation="wave"
                     variant="rect"
                     height={240}
-                    width="887px"
-                    className={cx('my-4', 'mobile:max-tablet:!w-[312px]', 'tablet:max-desktop:!w-[768px]')}
+                    className={cx('my-4', 'mobile:max-tablet:!w-full', 'tablet:max-desktop:!w-full', 'desktop:!w-[887px]')}
                 />
                 <Skeleton
                     animation="wave"
                     variant="rect"
                     height={240}
-                    width="887px"
-                    className={cx('my-4', 'mobile:max-tablet:!w-[312px]', 'tablet:max-desktop:!w-[768px]')}
+                    className={cx('my-4', 'mobile:max-tablet:!w-full', 'tablet:max-desktop:!w-full', 'desktop:!w-[887px]')}
                 />
                 <Skeleton
                     animation="wave"
                     variant="rect"
                     height={240}
-                    width="887px"
-                    className={cx('my-4', 'mobile:max-tablet:!w-[312px]', 'tablet:max-desktop:!w-[768px]')}
+                    className={cx('my-4', 'mobile:max-tablet:!w-full', 'tablet:max-desktop:!w-full', 'desktop:!w-[887px]')}
                 />
             </div>
         </div>
