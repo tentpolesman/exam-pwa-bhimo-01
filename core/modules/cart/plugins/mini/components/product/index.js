888ea52e29d9b14b266b42748f935a01969e3771
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2024-01-08 12:27:58 +0700

    feat([29365480] - PWA - Mini Cart): Fix loader test case filled - Feedback 95

--- core/modules/cart/plugins/mini/components/product/index.js
+++ core/modules/cart/plugins/mini/components/product/index.js
@@ -1,31 +1,29 @@
 import Item from '@plugin_minicart/components/product/item';
 
-import cx from 'classnames';
-import Typography from '@common_typography';
 import Button from '@common_button';
+import Typography from '@common_typography';
 import ShoppingCartIcon from '@heroicons/react/24/solid/ShoppingCartIcon';
+import cx from 'classnames';
 
 const ItemCart = (props) => {
     const {
-        data, deleteCart, updateCart, t, storeConfig, currencyCache,
+        // prettier-ignore
+        data,
+        deleteCart,
+        updateCart,
+        t,
+        storeConfig,
+        currencyCache,
     } = props;
 
     if (data.length === 0) {
         return (
             <div className="w-full h-[calc(100vh-20rem)] gap-4 flex flex-col align-middle justify-center items-center">
-                <ShoppingCartIcon className={cx(
-                    'w-10 h-10 tablet:w-14 tablet:h-14 text-neutral-300',
-                )}
-                />
-                <Typography className="font-normal text-[14px] tablet:text-base">
-                    {t('cart:empty:text')}
-                </Typography>
+                <ShoppingCartIcon className={cx('w-10 h-10 tablet:w-14 tablet:h-14 text-neutral-300')} />
+                <Typography className="font-normal text-[14px] tablet:text-base">{t('cart:empty:text')}</Typography>
                 <Button
                     variant="primary"
-                    className={cx(
-                        'justify-center',
-                        'h-full w-full max-h-[38px] tablet:max-h-[48px] max-w-[142px] tablet:max-w-[170px]',
-                    )}
+                    className={cx('justify-center', 'h-full w-full max-h-[38px] tablet:max-h-[48px] max-w-[142px] tablet:max-w-[170px]')}
                     link="/"
                 >
                     {t('cart:button:startShopping')}
@@ -33,16 +31,24 @@ const ItemCart = (props) => {
             </div>
         );
     }
-    let heightFinal = window.innerHeight - 240;
 
-    if (window.innerWidth < 768) {
-        heightFinal = window.innerHeight - 200;
-    } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
-        heightFinal = window.innerHeight - 220;
-    }
+    const [heightState, setHeight] = React.useState(500);
+
+    React.useEffect(() => {
+        if (typeof window !== 'undefined') {
+            if (window.innerWidth < 768) {
+                setHeight(window.innerHeight - 220);
+            } else {
+                setHeight(window.innerHeight - 260);
+            }
+
+            return () => {};
+        }
+        return null;
+    }, [window]);
 
     return (
-        <ol className={cx('overflow-y-auto')} style={{ height: heightFinal }}>
+        <ol className={cx('overflow-y-auto')} style={{ height: heightState }}>
             {data.map((val, idx) => (
                 <Item {...val} key={idx} deleteCart={deleteCart} updateCart={updateCart} storeConfig={storeConfig} currencyCache={currencyCache} />
             ))}
