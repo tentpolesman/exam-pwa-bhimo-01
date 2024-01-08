ff486efe092767e0517ac5c96c0f6dc95492c299
zakysirclo <zaky.arifudin@sirclo.com>
2024-01-08 17:31:45 +0700

    mini cart wording except cart page

--- core/modules/cart/plugins/mini/components/product/index.js
+++ core/modules/cart/plugins/mini/components/product/index.js
@@ -20,13 +20,13 @@ const ItemCart = (props) => {
         return (
             <div className="w-full h-[calc(100vh-20rem)] gap-4 flex flex-col align-middle justify-center items-center">
                 <ShoppingCartIcon className={cx('w-10 h-10 tablet:w-14 tablet:h-14 text-neutral-300')} />
-                <Typography className="font-normal text-[14px] tablet:text-base">{t('cart:empty:text')}</Typography>
+                <Typography className="font-normal text-[14px] tablet:text-base">{t('common:cart:emptyCart')}</Typography>
                 <Button
                     variant="primary"
                     className={cx('justify-center', 'h-full w-full max-h-[38px] tablet:max-h-[48px] max-w-[142px] tablet:max-w-[170px]')}
                     link="/"
                 >
-                    {t('cart:button:startShopping')}
+                    {t('common:button:startShopping')}
                 </Button>
             </div>
         );
