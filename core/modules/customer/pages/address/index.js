19b8ad18daead77ac9a6077068b706a3c8e998b8
zakysirclo <zaky.arifudin@sirclo.com>
2024-01-09 17:48:15 +0700

    handling error -> add,edit, remove

--- core/modules/customer/pages/address/index.js
+++ core/modules/customer/pages/address/index.js
@@ -1,6 +1,8 @@
 import { withApollo } from '@lib_apollo';
 import { withTranslation } from 'next-i18next';
-import Core from '@core_modules/customer/pages/address/core';
+import dynamic from 'next/dynamic';
+
+const Core = dynamic(() => import('@core_modules/customer/pages/address/core'), { ssr: false });
 
 const Page = (props) => <Core {...props} />;
 
