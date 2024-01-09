ec950ad03d825f750a5f6e7cdeb3083636ae47cb
sureyeahdie <sureyeahdie@gmail.com>
2024-01-09 13:15:17 +0700

    update translation imports

--- core/commons/Forms/Radio/index.js
+++ core/commons/Forms/Radio/index.js
@@ -10,7 +10,7 @@ import cx from 'classnames';
 import parser from 'html-react-parser';
 import propTypes from 'prop-types';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 
 const Radio = (props) => {
     const {
