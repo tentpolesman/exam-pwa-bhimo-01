7dbd9272f9e3a72fddb2b1b92f50dcd74c5646a2
zakysirclo <zaky.arifudin@sirclo.com>
2024-01-15 11:36:54 +0700

    fix logo checkout pecah

--- core/modules/checkout/pages/default/components/Header/index.js
+++ core/modules/checkout/pages/default/components/Header/index.js
@@ -6,9 +6,7 @@ import Router from 'next/router';
 import { getAppEnv } from '@root/core/helpers/env';
 import Image from '@common_image';
 
-const HeaderCheckout = ({
-    storeConfig,
-}) => {
+const HeaderCheckout = ({ storeConfig }) => {
     const BackToStore = () => {
         if (modules.checkout.checkoutOnly) {
             window.location.replace(getStoreHost(getAppEnv()));
@@ -19,21 +17,17 @@ const HeaderCheckout = ({
 
     return (
         <div id="header" className="flex flex-col justify-center items-center w-full h-44">
-            <button
-                className="border-none w-max"
-                onClick={BackToStore}
-                type="button"
-            >
-                <Image
-                    width={230}
-                    height={68}
-                    src={`${storeConfig.secure_base_media_url}logo/${storeConfig.header_logo_src}`}
-                    alt="logo"
-                    className="!w-[230px] !h-[68px]"
-                    classContainer="!w-[230px] !h-[68px]"
-                    quality={100}
-                    storeConfig={storeConfig}
-                />
+            <button className="border-none w-max" onClick={BackToStore} type="button">
+                <div className="!w-[110px] !h-[34px]">
+                    <Image
+                        width={110}
+                        height={34}
+                        src={`${storeConfig.secure_base_media_url}logo/${storeConfig.header_logo_src}`}
+                        alt="logo"
+                        quality={100}
+                        storeConfig={storeConfig}
+                    />
+                </div>
             </button>
         </div>
     );
