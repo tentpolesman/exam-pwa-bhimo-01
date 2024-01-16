661acb1384e69470260895d5befe08ab2ae8cd7e
zakysirclo <zaky.arifudin@sirclo.com>
2024-01-16 11:42:28 +0700

    fix product compare style & downloadable in compare

--- core/modules/productcompare/pages/default/components/index.js
+++ core/modules/productcompare/pages/default/components/index.js
@@ -5,9 +5,10 @@ import Typography from '@common_typography';
 import ConfirmationDelete from '@core_modules/cart/pages/default/components/confirmDelete';
 import Empty from '@core_modules/productcompare/pages/default/components/empty';
 import ProductItem from '@plugin_productitem';
-import classNames from 'classnames';
+import cx from 'classnames';
 import React from 'react';
 import ClearIcon from '@heroicons/react/24/outline/XMarkIcon';
+import Show from '@common_show';
 
 const Content = (props) => {
     const {
@@ -54,11 +55,11 @@ const Content = (props) => {
                     <thead className="table-header-group">
                         <tr>
                             <th
-                                className={classNames(
+                                className={cx(
                                     'hidden tablet:table-cell',
                                     'sticky left-0 z-[9] bg-neutral-white',
                                     'border-r border-r-neutral-200',
-                                    'align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                    'align-top border-b border-b-neutral-200 tracking-widest',
                                 )}
                                 style={{ width: columnWidth }}
                                 scope="col"
@@ -66,27 +67,27 @@ const Content = (props) => {
                                 {' '}
                             </th>
                             {compareList.compareList.items.map((productCompare) => {
-                                const { product, product_id } = productCompare;
+                                const { product } = productCompare;
                                 return (
                                     <th
-                                        align="center"
-                                        className={classNames(
+                                        className={cx(
                                             'min-w-64',
-                                            'table-cell p-4 align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                            'table-cell p-4 align-top border-b border-b-neutral-200 tracking-widest',
+                                            'border-r border-r-neutral-200',
                                         )}
                                         style={{ width: columnWidth }}
                                     >
-                                        <div className={classNames(
-                                            'w-max relative h-auto',
-                                        )}
-                                        >
+                                        <div className={cx('!w-[288px] relative h-auto, text-left')}>
                                             <ProductItem
                                                 {...product}
                                                 enableProductCompare={false}
                                                 storeConfig={storeConfig}
-                                                className="shadow-none border-none"
+                                                className="!shadow-none !border-none !hover:shadow-none !hover:border-none"
                                             />
-                                            <ClearIcon className="cursor-pointer absolute w-5 h-5 top-1 right-1 z-[1]" onClick={() => confirmDelete(productCompare)} />
+                                            <ClearIcon
+                                                className="cursor-pointer absolute w-5 h-5 top-1 right-1 z-[1]"
+                                                onClick={() => confirmDelete(productCompare)}
+                                            />
                                         </div>
                                     </th>
                                 );
@@ -96,11 +97,11 @@ const Content = (props) => {
                     <tbody>
                         <tr>
                             <td
-                                className={classNames(
+                                className={cx(
                                     'hidden tablet:table-cell',
                                     'sticky left-0 z-[9] bg-neutral-white',
                                     'border-r border-r-neutral-200',
-                                    'align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                    'align-top border-b border-b-neutral-200 tracking-widest',
                                     'p-4',
                                 )}
                                 style={{ width: columnWidth }}
@@ -113,17 +114,16 @@ const Content = (props) => {
                                 const { product } = productCompare;
                                 return (
                                     <td
-                                        className={classNames(
+                                        className={cx(
                                             'min-w-64',
+                                            'border-r border-r-neutral-200',
                                             'border-b border-b-neutral-200',
                                             'p-4',
+                                            'align-top',
                                         )}
                                         style={{ width: columnWidth }}
                                     >
-                                        <Typography
-                                            variant="bd-3"
-                                            className={classNames('uppercase desktop:hidden')}
-                                        >
+                                        <Typography variant="bd-3" className={cx('uppercase desktop:hidden')}>
                                             {t('common:productCompare:label:sku')}
                                         </Typography>
                                         {product.sku}
@@ -133,11 +133,11 @@ const Content = (props) => {
                         </tr>
                         <tr>
                             <td
-                                className={classNames(
+                                className={cx(
                                     'hidden tablet:table-cell',
                                     'sticky left-0 z-[9] bg-neutral-white',
                                     'border-r border-r-neutral-200',
-                                    'align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                    'align-top border-b border-b-neutral-200 tracking-widest',
                                     'p-4',
                                 )}
                                 style={{ width: columnWidth }}
@@ -148,32 +148,39 @@ const Content = (props) => {
                                 const { product } = productCompare;
                                 return (
                                     <td
-                                        className={classNames(
+                                        className={cx(
                                             'min-w-64',
+                                            'border-r border-r-neutral-200',
                                             'border-b border-b-neutral-200',
                                             'p-4',
+                                            'align-top',
                                         )}
                                         style={{ width: columnWidth }}
                                     >
                                         <Typography variant="bd-3" className="flex desktop:hidden">
                                             {t('common:productCompare:label:description')}
                                         </Typography>
-                                        <div
-                                            className="description-item"
-                                            style={{ width: columnWidth }}
-                                            dangerouslySetInnerHTML={{ __html: product.description.html }}
-                                        />
+                                        <Show when={!product.description.html}>
+                                            <>-</>
+                                        </Show>
+                                        <Show when={product.description.html}>
+                                            <div
+                                                className="description-item"
+                                                style={{ width: columnWidth }}
+                                                dangerouslySetInnerHTML={{ __html: product.description.html }}
+                                            />
+                                        </Show>
                                     </td>
                                 );
                             })}
                         </tr>
                         <tr>
                             <td
-                                className={classNames(
+                                className={cx(
                                     'hidden tablet:table-cell',
                                     'sticky left-0 z-[9] bg-neutral-white',
                                     'border-r border-r-neutral-200',
-                                    'align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                    'align-top border-b border-b-neutral-200 tracking-widest',
                                     'p-4',
                                 )}
                                 style={{ width: columnWidth }}
@@ -184,51 +191,64 @@ const Content = (props) => {
                                 const { product } = productCompare;
                                 return (
                                     <td
-                                        className={classNames(
+                                        className={cx(
                                             'min-w-64',
+                                            'border-r border-r-neutral-200',
                                             'border-b border-b-neutral-200',
                                             'p-4',
+                                            'align-top',
                                         )}
                                         style={{ width: columnWidth }}
                                     >
                                         <Typography variant="bd-3" className="flex desktop:hidden">
                                             {t('common:productCompare:label:shortDescription')}
                                         </Typography>
-
-                                        <div dangerouslySetInnerHTML={{ __html: product.short_description.html }} />
+                                        <Show when={!product.short_description.html}>
+                                            <>-</>
+                                        </Show>
+                                        <Show when={product.short_description.html}>
+                                            <div dangerouslySetInnerHTML={{ __html: product.short_description.html }} />
+                                        </Show>
                                     </td>
                                 );
                             })}
                         </tr>
-                        <tr>
+                        {/** brand name not available on gql, only brand id */}
+                        {/* <tr>
                             <td
-                                className={classNames(
+                                className={cx(
                                     'hidden tablet:table-cell',
                                     'sticky left-0 z-[9] bg-neutral-white',
+                                    'border-t border-t-neutral-200',
                                     'border-r border-r-neutral-200',
-                                    'align-[inherit] border-b border-b-neutral-200 tracking-widest',
+                                    'align-top border-b border-b-neutral-200 tracking-widest',
                                     'p-4',
                                 )}
                                 style={{ width: columnWidth }}
                             >
                                 {t('common:productCompare:label:brand')}
                             </td>
-                            {compareList.compareList.items.map(() => (
-                                <td
-                                    className={classNames(
-                                        'min-w-64',
-                                        'border-b border-b-neutral-200',
-                                        'p-4',
-                                    )}
-                                    style={{ width: columnWidth }}
-                                >
-                                    <Typography variant="bd-3" className="flex desktop:hidden">
-                                        {t('common:productCompare:label:brand')}
-                                    </Typography>
-                                    {' '}
-                                </td>
-                            ))}
-                        </tr>
+                            {compareList.compareList.items.map((productCompare) => {
+                                const { product } = productCompare;
+                                return (
+                                    <td
+                                        className={cx(
+                                            'min-w-64',
+                                            'border-r border-r-neutral-200',
+                                            'border-b border-b-neutral-200',
+                                            'p-4',
+                                            'align-top',
+                                        )}
+                                        style={{ width: columnWidth }}
+                                    >
+                                        <Typography variant="bd-3" className="flex desktop:hidden">
+                                            {t('common:productCompare:label:brand')}
+                                        </Typography>
+                                        {product.brand ?? '-'}
+                                    </td>
+                                );
+                            })}
+                        </tr> */}
                     </tbody>
                 </table>
             </div>
