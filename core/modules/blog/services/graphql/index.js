6971805f9292abd141bf0c8a447ac638db104ac0
sureyeahdie <sureyeahdie@gmail.com>
2024-01-18 16:51:47 +0700

    delete unused schema

--- core/modules/blog/services/graphql/index.js
+++ core/modules/blog/services/graphql/index.js
@@ -1,25 +1,5 @@
-import { useQuery, useLazyQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import * as schema from '@core_modules/blog/services/graphql/schema';
-
-export const getBlog = (params = {}) => useQuery(schema.getAllPost, {
-    ...params,
-});
-
-export const getCategory = (variables) => useQuery(schema.getCategory, {
-    variables,
-});
-
-export const getDetailBlog = (id) => useQuery(schema.getPostById, {
-    variables: {
-        id,
-    },
-});
-
-export const awBlogPostWithRelatedPosts = (options = {}) => useLazyQuery(schema.awBlogPostWithRelatedPosts, {
-    ...options,
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'all',
-});
 
 export const getBlogPostList = () => useLazyQuery(schema.getBlogPostList);
 export const getBlogCategoryList = () => useLazyQuery(schema.getBlogCategoryList);
--- core/modules/blog/services/graphql/schema.js
+++ core/modules/blog/services/graphql/schema.js
@@ -1,45 +1,5 @@
 import { gql } from '@apollo/client';
 
-const itemBlog = `
-    id
-    author_id
-    title
-    blog_post_url
-    category_ids
-    short_content
-    content
-    url_key
-    created_at
-    updated_at
-    customer_groups
-    tag_names
-    status
-    publish_date
-    featured_image_url
-    meta_twitter_site
-    meta_keywords
-    meta_description
-    meta_title
-`;
-
-export const getCategory = gql`
-    query getCategory($category_id: Int, $url_key: String) {
-        getBlogCategory(category_id: $category_id, url_key: $url_key) {
-            data {
-                id
-                name
-                url_key
-                meta_description
-                meta_title
-                status
-                sort_order
-                updated_at
-                created_at
-            }
-        }
-    }
-`;
-
 export const getBlogCategoryList = gql`
     query getBlogCategoryList {
         getBlogCategoryList {
@@ -50,35 +10,6 @@ export const getBlogCategoryList = gql`
                 category_id
                 identifier
                 title
-            }
-        }
-    }
-`;
-
-export const getAllPost = gql`
-    query getAllBlog(
-        $page_size: Int,
-        $current_page: Int,
-        $category_id: Int,
-        $id: Int,
-        $url_key: String,
-
-    ){
-        getBlogByFilter(
-            page_size: $page_size,
-            current_page: $current_page,
-            filters: {
-                category_id: $category_id,
-                id: $id,
-                url_key: $url_key
-            }
-        ) {
-            page_size,
-            total_count
-            total_pages
-            current_page
-            items {
-             ${itemBlog}
             }
         }
     }
@@ -132,86 +63,6 @@ export const getBlogPostList = gql`
                 }
                 featured
                 position
-            }
-        }
-    }
-`;
-
-export const getPostById = gql`
-    query getBlog($id: Int) {
-        getBlogByFilter(id: $id) {
-            data {
-                id
-                title
-                author_id
-                url_key
-                short_content
-                publish_date
-                featured_image_url
-                featured_image_alt
-                content
-                created_at
-            }
-        }
-    }
-`;
-
-export const awBlogPostWithRelatedPosts = gql`
-    query awBlogPostWithRelatedPosts($postId: Int, $storeId: Int) {
-        awBlogPostWithRelatedPosts(postId: $postId, storeId: $storeId) {
-            related_product {
-                id
-                attribute_set_id
-                name
-                sku
-                small_image {
-                    url
-                }
-                url_key
-                price_range {
-                    maximum_price {
-                        discount {
-                            amount_off
-                            percent_off
-                        }
-                        final_price {
-                            currency
-                            value
-                        }
-                        fixed_product_taxes {
-                            amount {
-                                currency
-                                value
-                            }
-                            label
-                        }
-                        regular_price {
-                            currency
-                            value
-                        }
-                    }
-                    minimum_price {
-                        discount {
-                            amount_off
-                            percent_off
-                        }
-                        final_price {
-                            currency
-                            value
-                        }
-                        fixed_product_taxes {
-                            amount {
-                                currency
-                                value
-                            }
-                            label
-                        }
-                        regular_price {
-                            currency
-                            value
-                        }
-                    }
-                }
             }
         }
     }
