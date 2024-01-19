d379ba847eb985c5cce37596801c62cfa9b356e0
sureyeahdie <sureyeahdie@gmail.com>
2024-01-19 14:11:12 +0700

    revamp track order page

--- core/modules/trackingorder/pages/default/components/form/index.js
+++ core/modules/trackingorder/pages/default/components/form/index.js
@@ -2,17 +2,26 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
+import FormView from '@core_modules/trackingorder/pages/default/components/form/view';
+import DetailView from '@core_modules/trackingorder/plugins/ModalTrackOrder';
+import { checkJson } from '@core_modules/trackingorder/pages/default/helpers/checkJson';
+import dynamic from 'next/dynamic';
+import Typography from '@common/Typography';
+import cx from 'classnames';
+
+const Breadcrumb = dynamic(() => import('@common_breadcrumb'), { ssr: false });
 
 const FormCom = (props) => {
     const {
-        setOrderField, t, email, FormView, data, loading, error, SkeletonResult, DetailView, ResultView, getTrackOrder,
-        ...other
+        setOrderField, t, email, data, loading, error, getTrackOrder,
     } = props;
     const [openResult, setOpenResult] = React.useState(false);
     const [openModal, setOpenModal] = React.useState(false);
     const [modalType, setModalType] = React.useState('');
     const [modalData, setModalData] = React.useState('');
+
+    const breadcrumbsData = [{ label: t('trackingorder:trackingOrder'), link: '#', active: true }];
 
     const TrackingSchema = Yup.object().shape({
         email: Yup.string().email(t('validate:email:wrong')).required(t('validate:email:required')),
@@ -58,41 +67,62 @@ const FormCom = (props) => {
         },
     });
 
+    useEffect(() => {
+        if (!loading && !error && data?.ordersFilter?.data?.length > 0 && data.ordersFilter.data[0].detail?.length > 0) {
+            const shippingMethods = data.ordersFilter.data[0].detail[0].shipping_methods.shipping_detail;
+            let orderDetail = shippingMethods[0].data_detail;
+            orderDetail = orderDetail.replace(/'/g, '`');
+            orderDetail = orderDetail.replace(/"/g, "'");
+            orderDetail = orderDetail.replace(/`/g, '"');
+
+            if (checkJson(orderDetail) && !JSON.parse(orderDetail).errors) {
+                orderDetail = JSON.parse(orderDetail);
+                setOpenModal(true);
+                setModalType(shippingMethods[0].trackorder_type);
+                setModalData(orderDetail);
+            }
+        }
+
+        if (error || !data || data?.ordersFilter?.data?.length === 0) {
+            setOpenModal(true);
+            setModalType('');
+            setModalData('');
+        }
+    }, [data, error, loading]);
+
     const handleOpenResult = (val) => {
         setOpenResult(val);
     };
 
-    const handleOpenModal = (type, datas) => {
-        setOpenModal(true);
-        setModalType(type);
-        setModalData(datas);
-    };
-
     return (
         <>
-            <div className="flex flex-row">
-                <div className="lg:basis-1/2 md:basis-1/2 sm:basis-full xs:basis-full">
+            <Breadcrumb
+                withHome
+                iconHomeOnly
+                className={cx('mb-[40px]', 'desktop:mt-[10px]', 'tablet:mt-[0px]', 'mobile:mt-[16px] mobile:ml-[16px]')}
+                data={breadcrumbsData}
+            />
+            <Typography className="flex justify-center mb-10 mt-14 tablet:mt-0" variant="h1">
+                {t('trackingorder:trackingOrder')}
+            </Typography>
+            <div className="flex justify-center">
+                <div
+                    className="max-w-[400px] w-full tablet:w-[550px] tablet:max-w-[550px] desktop:w-[650px] desktop:max-w-[650px]
+                            rounded-md border border-neutral-200 bg-white flex-col gap-6 inline-flex
+                            mobile:mx-4 mx-auto px-3 pb-6 tablet:p-8 pt-5 "
+                >
                     <FormView {...props} formik={formik} handleOpenResult={handleOpenResult} openResult={openResult} />
                 </div>
-                <div className="lg:basis-1/2 md:basis-1/2 sm:basis-full xs:basis-full">
-                    {openResult ? (
-                        loading || !data ? (
-                            <SkeletonResult />
-                        ) : (
-                            <>
-                                {data.ordersFilter && <ResultView {...other} t={t} orders={data.ordersFilter} openModal={handleOpenModal} />}
-                                <DetailView
-                                    {...props}
-                                    modalType={modalType}
-                                    modalData={modalData}
-                                    open={openModal}
-                                    setOpen={setOpenModal}
-                                    orders={data.ordersFilter}
-                                />
-                            </>
-                        )
-                    ) : null}
-                </div>
+                {!loading && (data || modalData) ? (
+                    <DetailView
+                        {...props}
+                        modalType={modalType}
+                        modalData={modalData}
+                        open={openModal}
+                        setOpen={setOpenModal}
+                        orders={data.ordersFilter}
+                    />
+                ) : null}
             </div>
         </>
     );
--- core/modules/trackingorder/pages/default/core.js
+++ core/modules/trackingorder/pages/default/core.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import Layout from '@layout';
 import { getCustomer, getTrackingOrder } from '@core_modules/trackingorder/services/graphql';
-import Content from '@core_modules/trackingorder/pages/default/components/form';
+import dynamic from 'next/dynamic';
+
+const Skeleton = dynamic(() => import('@core_modules/trackingorder/pages/default/components/skeletonform'), { ssr: false });
+const Content = dynamic(() => import('@core_modules/trackingorder/pages/default/components/form'), { ssr: false });
 
 const Tracking = (props) => {
     let customer = {};
-    const {
-        isLogin, Skeleton, pageConfig, t,
-    } = props;
+    const { isLogin, t } = props;
     const config = {
         title: t('trackingorder:trackingOrder'),
         header: 'relative', // available values: "absolute", "relative", false (default)
@@ -25,7 +26,7 @@ const Tracking = (props) => {
         const { data: dataCustomer, loading: loadCustomer } = getCustomer();
         if (loadCustomer) {
             return (
-                <Layout {...props} pageConfig={pageConfig || config}>
+                <Layout {...props} pageConfig={config}>
                     <Skeleton />
                 </Layout>
             );
@@ -35,7 +36,7 @@ const Tracking = (props) => {
         }
     }
     return (
-        <Layout {...props} pageConfig={pageConfig || config}>
+        <Layout {...props} pageConfig={config}>
             <Content
                 {...props}
                 email={customer.email || ''}
@@ -58,10 +59,10 @@ Tracking.propTypes = {
 };
 
 Tracking.defaultProps = {
-    FormView: () => { },
-    Skeleton: () => { },
-    SkeletonResult: () => { },
-    ResultView: () => { },
+    FormView: () => {},
+    Skeleton: () => {},
+    SkeletonResult: () => {},
+    ResultView: () => {},
 };
 
 export default Tracking;
