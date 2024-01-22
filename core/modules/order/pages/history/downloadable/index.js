acab2cd334eb2f4974422c8cb234ba44af4e33a5
zakysirclo <zaky.arifudin@sirclo.com>
2024-01-22 13:15:48 +0700

    my downloadable : tablet & mobile

--- core/modules/order/pages/history/downloadable/index.js
+++ core/modules/order/pages/history/downloadable/index.js
@@ -2,11 +2,7 @@ import { withTranslation } from 'next-i18next';
 import { withApollo } from '@lib_apollo';
 import Core from '@core_modules/order/pages/history/downloadable/core';
 import Content from '@core_modules/order/pages/history/components/downloadable';
-import Skeleton from '@core_modules/order/pages/history/components/skeleton';
-import ErrorView from '@core_modules/order/pages/history/components/error';
 
-const DefaultOrder = (props) => (
-    <Core {...props} Content={Content} Skeleton={Skeleton} ErrorView={ErrorView} />
-);
+const DefaultOrder = (props) => <Core {...props} Content={Content} />;
 
 export default withApollo({ ssr: true })(withTranslation()(DefaultOrder));
